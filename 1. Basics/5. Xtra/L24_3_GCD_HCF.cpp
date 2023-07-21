0aaa79a57945b594f6fba7906a5ea3bea5a1ab67
Pratyush Raj <pratyush2331@gmail.com>
2023-07-21 11:19:02 +0530

    updated GCD

--- 1. Basics/5. Xtra/L24_3_GCD_HCF.cpp
+++ 1. Basics/5. Xtra/L24_3_GCD_HCF.cpp
@@ -3,20 +3,18 @@
 #include<iostream>
 using namespace std;
 
-// Naive Approach
+// Naive Approach --> will give TLE
 /*
 int gcd(int a, int b) {
     if(a == 0) return b;
     if(b == 0) return a;
-
-    int ans = 1;
-    int mini = a<b ? a : b;
-    for(int i = 1; i <= mini; i++) {
-        if((a%i == 0) && (b%i == 0)) {
-            ans = i;
-        }
+    
+    int mini = min(a,b);
+    for(int i = mini; i >= 2; i--) {
+        if(a%i == 0 && b%i == 0) return i;
     }
-    return ans;
+    
+    return 1;
 }
 */
 
@@ -28,15 +26,11 @@ int gcd(int a, int b) {
     if(a == 0) return b;
     if(b == 0) return a;
 
-    while(a != b) { // while(a != 0 && b != 0) {            Reason: kyonki koi ek number dusre k equal ho k 0 bnega
-        if(a > b) {
-            a = a - b;
-        }
-        else {
-            b = b - a;
-        }
+    while(a != 0 && b != 0) {
+        if(a > b) a = a-b;
+        else b = b-a;
     }
-    return a;
+    return max(a,b);
 }
 */
 
@@ -50,14 +44,10 @@ int gcd(int a, int b) {
     if(b == 0) return a;
 
     while(a != 0 && b != 0) {
-        if(a > b) {
-            a = a % b;
-        }
-        else {
-            b = b % a;
-        }
+        if(a > b) a = a%b;
+        else b = b%a;
     }
-    return a>b ? a : b;
+    return max(a,b);
 }
 // */
 
--- try.cpp
+++ try.cpp
@@ -3,7 +3,33 @@
 #include<iostream>
 using namespace std;
 
+// Euclid's Algo
+int GCD(int a, int b) {
+
+}
+
+// normal method
+/*
+int GCD(int a, int b) {
+    if(a == 0) return b;
+    if(b == 0) return a;
+
+    int mini = min(a,b);
+
+    for(int i = 2; i <= mini; i++) {
+        if(a%i == 0 && b%i == 0)
+            return i;
+    }
+
+    return 1;
+}
+*/
+
 int main() {
+    int a, b;
+    cin >> a >> b;
+
+    cout << GCD(a, b);
     
     return 0;
 }
