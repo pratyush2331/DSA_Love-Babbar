e8930bc2671a5e6b631af99205bc87aa9511156e
Pratyush Raj <pratyush2331@gmail.com>
2023-07-29 12:12:07 +0530

    third commit

---
+++ 16. DP/3. 2D DP/L136_1_longestPalindromicSubsequence.cpp
@@ -0,0 +1,51 @@
+// 516. Longest Palindromic Subsequence
+// LeetCode : https://leetcode.com/problems/longest-palindromic-subsequence/
+
+#include<iostream>
+using namespace std;
+
+
+class Solution {
+    // method-3 : space optimization code of LCS
+    /*
+    TC : O(m*n)
+    SC : O(min(m,n))
+    */
+    int longestCommonSubsequence(string a, string b) {
+        // to reduce SC further from O(n) to O(min(m,n))
+        if(a.length() < b.length()) swap(a,b);
+
+        vector<int> curr(b.length()+1, 0);
+        vector<int> next(b.length()+1, 0);
+
+        for(int i = a.length()-1; i >= 0; i--) {
+            for(int j = b.length()-1; j >=0; j--) {
+                int ans = 0;
+
+                if(a[i] == b[j]) // matched
+                    ans = 1 + next[j+1];
+                else // not matched
+                    ans = max(next[j], curr[j+1]);
+                
+                curr[j] = ans;
+            }
+            next = curr;
+        }
+
+        return next[0];
+    }
+
+    public:
+    int longestPalindromeSubseq(string str) {
+        string revStr = str;
+        reverse(revStr.begin(), revStr.end());
+
+        return longestCommonSubsequence(str, revStr);
+    }
+};
+
+
+int main() {
+    
+    return 0;
+}
