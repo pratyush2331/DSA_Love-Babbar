fa0bb4203e8a1eeda8d70104252efb9067dd189a
Pratyush Raj <pratyush2331@gmail.com>
2023-08-01 19:05:19 +0530

    third commit

---
+++ 1. Basics/5. Xtra/L24_2_sieveOfEratosthenes.cpp
@@ -0,0 +1,67 @@
+// 204. Count Primes
+// LeetCode : https://leetcode.com/problems/count-primes/
+// Sieve of Eratosthenes
+
+/*
+total primes strictly less than n
+*/
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+// Method-2: Sieve of Eratosthenes, TC : O(n*log(logn))
+// /*
+int countPrimes(int n) {
+    vector<bool> prime(n+1, true); // sb ko prime kr de rhe h
+
+    prime[0] = prime[1] = false; // 0 aur 1 ko non-prime kr diye
+
+    int count = 0;
+    for(int i = 2; i < n; i++) {
+        if(prime[i]) {
+            // cout << i << " ";
+            count++;
+            for(int j = i*i; j < n; j = j+i) {
+                prime[j] = false;
+            }
+        }
+    }
+    return count;
+}
+// */
+
+
+// Method-1: Normal Method, TC : O(n*√n)
+/*
+bool isPrime(int n) {
+    for(int i = 2; i*i <= n; i++) {
+        if(n%i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int countPrimes(int n) {
+    int count = 0;
+    for(int i = 2; i < n; i++) {
+        if(isPrime(i)) {
+            count++;
+        }
+    }
+    return count;
+}
+*/
+
+
+int main() {
+    int n;
+    cout << "Enter n : ";
+    cin >> n;
+
+    cout << countPrimes(n) << endl;
+    
+    return 0;
+}
--- 1. Basics/5. Xtra/L24_HW_1_segmentedSieve.cpp
+++ 1. Basics/5. Xtra/L24_HW_1_segmentedSieve.cpp
@@ -10,57 +10,19 @@ constraints:
 R - L <= 10^6
 */
 
-#include<iostream>
-#include<vector>
-#include<cmath>
+#include <iostream>
+#include <vector>
+#include <cmath>
 using namespace std;
 
-vector<long long> prime;
 
-void sieve(int n) {
-    vector<bool>isPrime(n+1, 1);
-
-    isPrime[0] = 0;
-    isPrime[1] = 0;
-
-    for(long long i = 2; i <= n; i++) {
-        if(isPrime[i]) {
-            prime.push_back(i);
-            for(long long j = i*i; j <= n; j += i) {
-                isPrime[j] = false;
-            }
-        }
-    }
-}
-
-void segSieve(int L, int R) {
-    long long sq = sqrt(R);
-    sieve(sq);
-    vector<bool> Primes(R-L+1, 1);
-    for(long long p : prime) {
-        long long sm = (L/p)*p;
-        if(sm < L)
-            sm += p;
-        for(long long m = sm; m <= R; m += p) {
-            Primes[m-L] = 0;
-        }
-    }
-    int count = 0;
-    for(long long i = L; i <= R; i++) {
-        if(Primes[i-L] == true) {
-            count++;
-            cout << i << " ";
-        }
-    }
-    cout << "count = " << count << endl;
-}
 
 int main() {
-    long long L, R;
+    int L, R;
     cout << "Enter lower & upper limit : ";
     cin >> L >> R;
 
     segSieve(L, R);
-    
+
     return 0;
 }
