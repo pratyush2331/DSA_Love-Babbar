88f1292d31f2fc8cd68e92b75a43228ee932d790
Pratyush Raj <pratyush2331@gmail.com>
2023-08-02 14:26:25 +0530

    Added Greedy

---
+++ 17. Greedy/L140_2_maximumMeetingsInOneRoom.cpp
@@ -0,0 +1,42 @@
+// Maximum Meetings in One Room
+// LeetCode : https://practice.geeksforgeeks.org/problems/maximum-meetings-in-one-room/1
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+
+class Solution{
+    public:
+    vector<int> maxMeetings(int N, vector<int> &S, vector<int> &F){
+        vector<int> ans;
+        
+        vector<vector<int>> v(N, vector<int>(3));
+        for(int i = 0; i < N; i++) {
+            v[i][0] = F[i];
+            v[i][1] = S[i];
+            v[i][2] = i;
+        }
+        
+        sort(v.begin(), v.end());
+        ans.push_back(v[0][2] + 1);
+        int curr = v[0][0];
+        
+        for(int i = 1; i < N; i++) {
+            if(curr < v[i][1]) {
+                ans.push_back(v[i][2] + 1);
+                curr = v[i][0];
+            }
+        }
+        
+        sort(ans.begin(), ans.end());
+        return ans;
+    }
+};
+
+
+int main() {
+    
+    return 0;
+}
