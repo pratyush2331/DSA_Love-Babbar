45a1c5c59dddef4999727c35564f0b2d12e4e426
Pratyush Raj <pratyush2331@gmail.com>
2023-08-04 01:03:08 +0530

    Added Greedy

---
+++ 17. Greedy/L140_8_minimumCostOfRopes.cpp
@@ -0,0 +1,40 @@
+// Minimum Cost of ropes
+// GFG : https://practice.geeksforgeeks.org/problems/minimum-cost-of-ropes-1587115620/1
+
+#include<iostream>
+using namespace std;
+
+
+class Solution {
+    public:
+    //Function to return the minimum cost of connecting the ropes.
+    long long minCost(long long arr[], long long n) {
+        // Your code here
+        priority_queue<long long, vector<long long>, greater<long long>> pq;
+        for(int i = 0; i < n; i++) {
+            pq.push(arr[i]);
+        }
+        
+        long long cost = 0;
+        while(pq.size() > 1) {
+            long long a = pq.top();
+            pq.pop();
+            
+            long long b = pq.top();
+            pq.pop();
+            
+            long long sum = a + b;
+            cost += sum;
+            
+            pq.push(sum);
+        }
+        
+        return cost;
+    }
+};
+
+
+int main() {
+    
+    return 0;
+}
---
+++ 17. Greedy/L140_9_huffmanEncoding.cpp
@@ -0,0 +1,84 @@
+// Huffman Encoding
+// GFG : https://practice.geeksforgeeks.org/problems/huffman-encoding3345/1
+
+#include<iostream>
+using namespace std;
+
+
+// Node class
+class Node {
+    public:
+    int data;
+    Node* left;
+    Node* right;
+    
+    Node(int d) {
+        data = d;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+// custom comparator
+class cmp {
+    public:
+    bool operator()(Node* a, Node* b) {
+        return a->data > b->data;
+    }
+};
+
+class Solution {
+    // preorder traversal
+    void preorderTraversal(Node* root, string temp, vector<string>& ans) {
+        if(root->left == NULL && root->right == NULL) {
+            // imp
+            ans.push_back(temp);
+            return;
+        }
+        
+        // imp
+        preorderTraversal(root->left, temp + '0', ans);
+        preorderTraversal(root->right, temp + '1', ans);
+    }
+    
+	public:
+	vector<string> huffmanCodes(string S,vector<int> f,int N) {
+	    // Code here
+	    priority_queue<Node*, vector<Node*>, cmp> pq;
+	    for(int i = 0; i < N; i++) {
+	       // imp
+	        Node* temp = new Node(f[i]);
+	        pq.push(temp);
+	    }
+	    
+	    while(pq.size() > 1) {
+	        Node* left = pq.top();
+	        pq.pop();
+	        
+	        Node* right = pq.top();
+	        pq.pop();
+	        
+	        Node* temp = new Node(left->data + right->data);
+	        temp->left = left;
+	        temp->right = right;
+	        
+	        pq.push(temp);
+	    }
+	    
+	    Node* root = pq.top();
+	    pq.pop(); // not needed to pop
+	    
+	   // imp
+	    vector<string> ans;
+	    string temp = "";
+	    preorderTraversal(root, temp, ans);
+	    
+	    return ans;
+	}
+};
+
+
+int main() {
+    
+    return 0;
+}
