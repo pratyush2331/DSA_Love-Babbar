3f9e30272898119068e1f24e4cabc7eb24d23d90
Pratyush Raj <pratyush2331@gmail.com>
2023-08-10 17:50:32 +0530

    added DP on Grids

---
+++ 16. DP/4. DP on Grids/1_ninjasTraining.cpp
@@ -0,0 +1,158 @@
+// Ninja’s Training
+// LeetCode : https://www.codingninjas.com/studio/problems/ninja-s-training_3621003
+
+#include<iostream>
+using namespace std;
+
+
+// method-3 : space optimization
+/*
+TC : O(N*4*3) = O(N)
+SC : O(2*4) = O(1)
+*/
+// /*
+int ninjaTraining(int n, vector<vector<int>> &points) {
+    // Write your code here.
+    vector<int> prev(4, 0);
+    vector<int> curr(4, 0);
+
+    // from base case
+    for(int last = 0; last < 4; last++) {
+        for(int task = 0; task < 3; task++) {
+            if(task != last) {
+                prev[last] = max(prev[last], points[0][task]);
+            }
+        }
+    }
+
+    for(int day = 1; day < n; day++) {
+        for(int last = 0; last < 4; last++) {
+            for(int task = 0; task < 3; task++) {
+                if(task != last) {
+                    int point = points[day][task] + prev[task];
+                    curr[last] = max(curr[last], point);
+                }
+            }
+        }
+        prev = curr;
+    }
+
+    return prev[3];
+}
+// */
+
+
+
+// method-2 : tabulation
+/*
+TC : O(N*4*3) = )(N)
+SC : O(N*4) = O(N)
+*/
+/*
+int ninjaTraining(int n, vector<vector<int>> &points) {
+    // Write your code here.
+    vector<vector<int>> dp(n, vector<int>(4, 0));
+
+    // from base case
+    for(int last = 0; last < 4; last++) {
+        for(int task = 0; task < 3; task++) {
+            if(task != last) {
+                dp[0][last] = max(dp[0][last], points[0][task]);
+            }
+        }
+    }
+
+    for(int day = 1; day < n; day++) {
+        for(int last = 0; last < 4; last++) {
+            for(int task = 0; task < 3; task++) {
+                if(task != last) {
+                    int point = points[day][task] + dp[day-1][task];
+                    dp[day][last] = max(dp[day][last], point);
+                }
+            }
+        }
+    }
+
+    return dp[n-1][3];
+}
+*/
+
+
+
+// method-1 : recursion + memoization
+/*
+TC : O(N*4*3) = O(N)
+SC : O(N) + O(N*4) = O(N)
+*/
+/*
+int f(int day, int last, vector<vector<int>>& points, vector<vector<int>>& dp) {
+    if(day == 0) {
+        int maxi = 0;
+        for(int task = 0; task < 3; task++) {
+            if(task != last) {
+                maxi = max(maxi, points[0][task]);
+            }
+        }
+        return maxi;
+    }
+
+    if(dp[day][last] != -1)
+        return dp[day][last];
+
+    int maxi = 0;
+
+    for(int task = 0; task < 3; task++) {
+        if(task != last) {
+            int point = points[day][task] + f(day-1, task, points, dp);
+            maxi = max(maxi, point);
+        }
+    }
+
+    return dp[day][last] = maxi;
+}
+
+int ninjaTraining(int n, vector<vector<int>> &points) {
+    // Write your code here.
+    vector<vector<int>> dp(n, vector<int>(4, -1));
+    return f(n-1, 3, points, dp);
+}
+*/
+
+
+
+// method-0 : recursion --> will give TLE
+/*
+int f(int day, int last, vector<vector<int>>& points) {
+    if(day == 0) {
+        int maxi = 0;
+        for(int task = 0; task < 3; task++) {
+            if(task != last) {
+                maxi = max(maxi, points[0][task]);
+            }
+        }
+        return maxi;
+    }
+
+    int maxi = 0;
+
+    for(int task = 0; task < 3; task++) {
+        if(task != last) {
+            int point = points[day][task] + f(day-1, task, points);
+            maxi = max(maxi, point);
+        }
+    }
+
+    return maxi;
+}
+
+int ninjaTraining(int n, vector<vector<int>> &points) {
+    // Write your code here.
+    return f(n-1, 3, points);
+}
+*/
+
+
+int main() {
+    
+    return 0;
+}
---
+++ 16. DP/4. DP on Grids/2_uniquePaths.cpp
@@ -0,0 +1,140 @@
+// https://leetcode.com/problems/unique-paths/
+// LeetCode : 62. Unique Paths
+
+#include<iostream>
+using namespace std;
+
+
+class Solution {
+    // method-3 : space optimization
+    /*
+    TC : O(m * n)
+    SC : O(n)
+    */
+    // /*
+    public:
+    int uniquePaths(int m, int n) {
+        vector<int> prev(n, 0);
+        vector<int> curr(n, 0);
+        
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                // from base case
+                if(i == 0 && j == 0) {
+                    curr[0] = 1;
+                    continue;
+                }
+                
+                int up = 0;
+                if(i > 0)
+                    up = prev[j];
+                int left = 0;
+                if(j > 0)
+                    left = curr[j-1];
+
+                curr[j] = up + left;
+            }
+            prev = curr;
+        }
+
+        return prev[n-1];
+    }
+    // */
+
+
+
+    // method-2 : tabulation
+    /*
+    TC : O(m * n)
+    SC : O(m * n)
+    */
+    /*
+    public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                // from base case
+                if(i == 0 && j == 0) {
+                    dp[0][0] = 1;
+                    continue;
+                }
+                
+                int up = 0;
+                if(i > 0)
+                    up = dp[i-1][j];
+                int left = 0;
+                if(j > 0)
+                    left = dp[i][j-1];
+
+                dp[i][j] = up + left;
+            }
+        }
+
+        return dp[m-1][n-1];
+    }
+    */
+
+
+
+    // method-1 : recursion + memoization
+    /*
+    TC : O(m * n)
+    SC : O((m-1) + (n-1)) + O(m*n)
+    */
+    /*
+    int f(int i, int j, vector<vector<int>>& dp) {
+        if(i == 0 && j == 0)
+            return 1;
+        if(i < 0 || j < 0)
+            return 0;
+        
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        
+        int up = f(i-1, j, dp);
+        int left = f(i, j-1, dp);
+
+        return dp[i][j] = up + left;
+    }
+
+    public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return f(m-1, n-1, dp);
+    }
+    */
+
+
+
+    // method-0 : recursion --> will give TLE
+    /*
+    TC : O()
+    SC : O()
+    */
+    /*
+    int f(int i, int j) {
+        if(i == 0 && j == 0)
+            return 1;
+        if(i < 0 || j < 0)
+            return 0;
+        
+        int up = f(i-1, j);
+        int left = f(i, j-1);
+
+        return up + left;
+    }
+
+    public:
+    int uniquePaths(int m, int n) {
+        return f(m-1, n-1);
+    }
+    */
+};
+
+
+int main() {
+    
+    return 0;
+}
