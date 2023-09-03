09e3211cef8f031e4cd81d7800c64b5509010594
Pratyush Raj <pratyush2331@gmail.com>
2023-09-03 19:10:49 +0530

    added leetcode biweekly solutions

---
+++ XX. LeetCode Contest/Biweekly Contest/Biweekly Contest 112/Q1_2839_CheckIfStringsCanBeMadeEqualWithOperationsI.cpp
@@ -0,0 +1,32 @@
+// 2839. Check if Strings Can be Made Equal With Operations I
+// LeetCode : https://leetcode.com/problems/check-if-strings-can-be-made-equal-with-operations-i/
+
+// Topics : unordered_map
+
+#include<iostream>
+using namespace std;
+
+
+class Solution {
+public:
+    bool canBeEqual(string s1, string s2) {
+        unordered_map<char, int> mp11, mp12, mp21, mp22;
+        
+        for(int i = 0; i < s1.size(); i++) {
+            mp11[s1[i]]++;
+            mp21[s2[i]]++;
+            if(i&1) {
+                mp12[s1[i]]++;
+                mp22[s2[i]]++;
+            }
+        }
+        
+        return (mp11 == mp21) && (mp12 == mp22);
+    }
+};
+
+
+int main() {
+    
+    return 0;
+}
