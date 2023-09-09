7ff0bbf8e9bf6b9ff63e930266d662c11885585a
Pratyush Raj <pratyush2331@gmail.com>
2023-09-09 22:58:44 +0530

    added digit dp problem

--- 16. DP/5. Digit DP/countNumbersWithDigitSumEqalToX.cpp
+++ 16. DP/5. Digit DP/countNumbersWithDigitSumEqalToX.cpp
@@ -3,9 +3,13 @@
 // 1 <= X <= 180
 
 #include<iostream>
+#include<cstring>
 using namespace std;
 
-// Brute Force
+
+
+// method-0 : Brute Force --> will give TLE
+// ----------------------------------------------------------------------------------------------------
 bool check(int num, int X) {
     int sum_digit = 0;
     while(num) {
@@ -22,11 +26,68 @@ int bruteForce(int L, int R, int X) {
     }
     return cnt;
 }
+// ----------------------------------------------------------------------------------------------------
+
+
+// Digit DP using recursion
+// ----------------------------------------------------------------------------------------------------
+int solve(string& num, int n, int x, bool tight) {
+    if(n == 1) {
+        if(x >= 0 && x <= 9)
+            return 1;
+        return 0;
+    }
+
+    int ans = 0;
+    int ub = tight ? (num[(num.length() - n)] - '0') : 9;
+    for(int digit = 0; digit <= ub; digit++) {
+        ans += solve(num, n-1, x-digit, (tight & (digit == ub)));
+    }
+
+    return ans;
+}
+// ----------------------------------------------------------------------------------------------------
+
+
+// Digit DP using recursion + memoization
+// ----------------------------------------------------------------------------------------------------
+int dp[101][181][2];
+
+int solveDP(string& num, int n, int x, bool tight) {
+    if(n == 1) {
+        if(x >= 0 && x <= 9)
+            return 1;
+        return 0;
+    }
+
+    if(dp[n][x][tight] != -1)
+        return dp[n][x][tight];
+
+    int ans = 0;
+    int ub = tight ? (num[(num.length() - n)] - '0') : 9;
+    for(int digit = 0; digit <= ub; digit++) {
+        ans += solveDP(num, n-1, x-digit, (tight & (digit == ub)));
+    }
+
+    return dp[n][x][tight] = ans;
+}
+// ----------------------------------------------------------------------------------------------------
+
+
 
 int main() {
-    int L = 1, R = 15, X = 5;
+    string L = "1", R = "1504456745678975";
+    int X = 5;
 
-    cout << bruteForce(L, R, X) << endl;
+    // Brute Force --> will give TLE
+    // cout << bruteForce(stoi(L), stoi(R), X) << endl;
+
+    // using recursion --> will give TLE
+    // cout << solve(R, R.length(), X, 1) << endl;
+
+    // using dp (recursion + memoization)
+    memset(dp, -1, sizeof(dp));
+    cout << solveDP(R, R.length(), X, 1) << endl;
     
     return 0;
 }
