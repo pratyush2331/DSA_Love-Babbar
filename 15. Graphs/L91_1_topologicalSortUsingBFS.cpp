b2f61788146a573062fd4f7ef8b59dd0cbb10011
Pratyush Raj <pratyush2331@gmail.com>
2023-09-28 23:46:27 +0530

    code updated

--- 15. Graphs/L91_1_topologicalSortUsingBFS.cpp
+++ 15. Graphs/L91_1_topologicalSortUsingBFS.cpp
@@ -10,50 +10,48 @@ using namespace std;
 TC : O(V+E)
 SC : O(V+E)
 */
-vector<int> topologicalSort(vector<vector<int>> &edges, int v, int e) {
-    // Write your code here
 
+vector<int> topologicalSort(vector<vector<int>> &edges, int v, int e)  {
     // creat adjList
-    unordered_map<int, list<int>> adjList;
+    vector<vector<int>> adjList(v);
     for(int i = 0; i < e; i++) {
         int u = edges[i][0];
         int v = edges[i][1];
-
+        
         adjList[u].push_back(v);
     }
 
     // find all indegrees
     vector<int> indegree(v);
-    for(auto i : adjList) {
-        for(auto j : i.second) {
-            indegree[j]++;
+    for(int i = 0; i < v; i++) {
+        for(int nbr : adjList[i]) {
+            indegree[nbr]++;
         }
     }
 
     // push all nodes with 0 indegree into queue
     queue<int> q;
     for(int i = 0; i < v; i++) {
-        if(indegree[i] == 0)
-            q.push(i);
+        if(!indegree[i]) q.push(i);
     }
 
     // do BFS
     vector<int> ans;
     while(!q.empty()) {
-        int frontNode = q.front();
+        int top = q.front();
         q.pop();
 
         // imp : store ans
         ans.push_back(frontNode);
 
         // decrement neighbour's indegree by 1
-        for(int nbr : adjList[frontNode]) {
+        for(int nbr : adjList[top]) {
             indegree[nbr]--;
-            if(indegree[nbr] == 0)
+            if(!indegree[nbr]) {
                 q.push(nbr);
+            }
         }
     }
-
     return ans;
 }
 
