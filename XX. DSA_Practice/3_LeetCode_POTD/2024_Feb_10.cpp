f434aba1b55085c06b7dea18165d05307534f62f
Pratyush Raj <pratyush2331@gmail.com>
2024-02-12 09:22:20 +0530

    added dp code

--- XX. DSA_Practice/3_LeetCode_POTD/2024_Feb_10.cpp
+++ XX. DSA_Practice/3_LeetCode_POTD/2024_Feb_10.cpp
@@ -5,6 +5,46 @@
 
 using namespace std;
 
+// /*
+// Method-2 : DP
+// TC: O(n^2), SC: O(n^2)
+class Solution {
+public:
+int countSubstrings(string str) {
+    int cnt = 0;
+
+    int n = str.length();
+    vector<vector<bool>> dp(n, vector<bool>(n, 0));
+
+    // initializing for 1 length
+    for(int i = 0; i < n; i++) {
+        dp[i][i] = 1;
+        cnt++;
+    }
+
+    // initializing for 2 length
+    for(int i = 0; i < n-1; i++) {
+        if(str[i] == str[i+1]) {
+            dp[i][i+1] = 1;
+            cnt++;
+        }
+    }
+
+    // Now, do DP tabulation
+    for(int len = 3; len <= n; len++) {
+        for(int i = 0; i < n-len+1; i++) {
+            if(dp[i+1][i+len-2] && str[i] == str[i+len-1]) {
+                dp[i][i+len-1] = 1;
+                cnt++;
+            }
+        }
+    }
+    
+    return cnt;
+}
+};
+// */
+
 
 // /*
 // Method-1 : Optimal Approach - iterating to every index of string and moving left and right pointer to check palindrome
