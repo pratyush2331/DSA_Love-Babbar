36d3655a7c0bfe3e487a3d3afb49628fe722e437
Pratyush Raj <pratyush2331@gmail.com>
2024-02-29 22:35:34 +0530

    solved 2 gfg problems

---
+++ XX. DSA_Practice/1_Sheets/Coder Army/Array/Easy_ThreeWayPartitioning.cpp
@@ -0,0 +1,32 @@
+// Three way partitioning
+// GFG : https://www.geeksforgeeks.org/problems/three-way-partitioning/1
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+class Solution{   
+public:
+    //Function to partition the array around the range such 
+    //that array is divided into three parts.
+    void threeWayPartition(vector<int>& arr,int a, int b) {
+        int n = arr.size();
+        int i = 0, j = 0, k = n-1;
+        while(j <= k) {
+            if(arr[j] < a) swap(arr[i++], arr[j++]);
+            else if(arr[j] >= a && arr[j] <= b) j++;
+            else if(arr[j] > b) swap(arr[j], arr[k--]);
+        }
+        
+        // for(auto ele : arr) cout << ele << " ";
+        // cout << "\n";
+    }
+};
+
+
+int main() {
+    
+
+    return 0;
+}
---
+++ XX. DSA_Practice/1_Sheets/Coder Army/Array/Easy_UnionOfTwoSortedArrays.cpp
@@ -0,0 +1,47 @@
+// Union of Two Sorted Arrays
+// GFG : https://www.geeksforgeeks.org/problems/union-of-two-sorted-arrays-1587115621/1
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+class Solution{
+    public:
+    //Function to return a list containing the union of the two arrays. 
+    vector<int> findUnion(int arr1[], int arr2[], int n, int m)
+    {
+        vector<int> ans;
+        int i = 0, j = 0;
+        while(i < n && j < m) {
+            if(arr1[i] == arr2[j]) {
+                if(ans.size() == 0 || ans[ans.size()-1] != arr1[i]) ans.push_back(arr1[i]);
+                i++; j++;
+            }
+            else if(arr1[i] < arr2[j]) {
+                if(ans.size() == 0 || ans[ans.size()-1] != arr1[i]) ans.push_back(arr1[i]);
+                i++;
+            }
+            else {
+                if(ans.size() == 0 || ans[ans.size()-1] != arr2[j]) ans.push_back(arr2[j]);
+                j++;
+            }
+        }
+        while(i < n) {
+            if(ans.size() == 0 || ans[ans.size()-1] != arr1[i]) ans.push_back(arr1[i]);
+            i++;
+        }
+        while(j < m) {
+            if(ans.size() == 0 || ans[ans.size()-1] != arr2[j]) ans.push_back(arr2[j]);
+            j++;
+        }
+        return ans;
+    }
+};
+
+
+int main() {
+    
+
+    return 0;
+}
