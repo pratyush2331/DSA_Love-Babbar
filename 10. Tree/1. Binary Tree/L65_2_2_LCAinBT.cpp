b28c65af3a1dcd3e4479697cdbc6989de6705a63
Pratyush Raj <pratyush2331@gmail.com>
2024-03-26 00:13:10 +0530

    code updated

--- 10. Tree/1. Binary Tree/L65_2_2_LCAinBT.cpp
+++ 10. Tree/1. Binary Tree/L65_2_2_LCAinBT.cpp
@@ -27,17 +27,14 @@ class Solution
     //Function to return the lowest common ancestor in a Binary Tree.
     Node* lca(Node* root ,int n1 ,int n2 ) 
     {
-        if(root == NULL) return NULL;
+        if(root == NULL || root->data == n1 || root->data == n2) return root;
         
-        if(root->data == n1 || root->data == n2) return root;
+        Node* left = lca(root->left, n1, n2);
+        Node* right = lca(root->right, n1, n2);
         
-        Node* leftAns = lca(root->left, n1, n2);
-        Node* rightAns = lca(root->right, n1, n2);
-        
-        if(leftAns != NULL && rightAns != NULL) return root;
-        else if(leftAns != NULL && rightAns == NULL) return leftAns;
-        else if(leftAns == NULL && rightAns != NULL) return rightAns;
-        else return NULL;
+        if(left == NULL) return right;
+        else if(right == NULL) return left;
+        else return root;
     }
 };
 
