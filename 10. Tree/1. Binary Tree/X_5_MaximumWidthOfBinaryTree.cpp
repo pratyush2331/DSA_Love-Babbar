b88a88c6b2890c015d5b9aae38adb5f4d129b4ab
Pratyush Raj <pratyush2331@gmail.com>
2024-03-29 19:44:45 +0530

    solved 1 LC Tree medium problem

---
+++ 10. Tree/1. Binary Tree/X_5_MaximumWidthOfBinaryTree.cpp
@@ -0,0 +1,41 @@
+// 662. Maximum Width of Binary Tree
+// LeetCode : https://leetcode.com/problems/maximum-width-of-binary-tree
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+class Solution {
+public:
+    int widthOfBinaryTree(TreeNode* root) {
+        int ans = 0;
+
+        if(root == NULL) return ans;
+
+        // idx starts from 0
+        queue<pair<TreeNode*,int>> q; // {node, idx}
+        q.push({root, 0});
+        while(!q.empty()) {
+            int x = q.size();
+            int first = q.front().second, last = q.back().second;
+            ans = max(ans, last-first+1);
+            while(x--) {
+                TreeNode* node = q.front().first;
+                int node_idx = q.front().second - first;
+                q.pop();
+                if(node->left) q.push({node->left, 2ll * node_idx + 1});
+                if(node->right) q.push({node->right, 2ll * node_idx + 2});
+            }
+        }
+
+        return ans;
+    }
+};
+
+
+int main() {
+    
+
+    return 0;
+}
