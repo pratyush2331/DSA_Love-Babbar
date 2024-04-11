e77a559dbb9f3e02565bd773d6d0b7a21380c277
Pratyush Raj <pratyush2331@gmail.com>
2024-04-12 01:54:53 +0530

    updated code

--- try.cpp
+++ try.cpp
@@ -4,25 +4,6 @@
 
 using namespace std;
 
-vector<int> preorder(TreeNode* root) {
-    vector<int> ans;
-    if(root == NULL) return ans;
-
-    stack<TreeNode*> st;
-    st.push(root);
-    while(!st.empty()) {
-        TreeNode* top = st.top();
-        st.pop();
-
-        ans.push_back(top->val);
-
-        if(top->right) st.push(top->right);
-        if(top->left) st.push(top->left);
-    }
-    
-    return ans;
-}
-
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
