613ed0fd717c2bc256d6d64fa0e608fbc61307af
Pratyush Raj <pratyush2331@gmail.com>
2024-04-23 20:37:31 +0530

    solved 1 LC easy BST Question

---
+++ 10. Tree/2. BST/X_1_MinimumAbsoluteDifferenceInBST.cpp
@@ -0,0 +1,32 @@
+// 530. Minimum Absolute Difference in BST
+// LeetCode : https://leetcode.com/problems/minimum-absolute-difference-in-bst
+
+// Revisit
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+    int mini = INT_MAX;
+    TreeNode* prev = NULL;
+    void dfsTraversal(TreeNode* root) {
+        if(root == NULL) return;
+        dfsTraversal(root->left);
+        if(prev) mini = min(mini, root->val - prev->val);
+        prev = root;
+        dfsTraversal(root->right);
+    }
+
+public:
+    int getMinimumDifference(TreeNode* root) {
+        dfsTraversal(root);
+        return mini;
+    }
+};
+
+
+int main() {
+
+    return 0;
+}
