c51df9736d223a60550efe30a44eebf94fe49419
Pratyush Raj <pratyush2331@gmail.com>
2024-04-25 22:51:32 +0530

    solved 1 LC easy BST Question

--- 10. Tree/2. BST/L69_2_searchInBST.cpp
+++ 10. Tree/2. BST/L69_2_searchInBST.cpp
@@ -1,63 +1,29 @@
-// Search In BST
-// CodeStudio : https://www.codingninjas.com/codestudio/problems/search-in-bst_1402878
+// 700. Search in a Binary Search Tree
+// LeetCode : https://leetcode.com/problems/search-in-a-binary-search-tree
 
 #include <bits/stdc++.h> 
 using namespace std;
 
-/*
-    Following is the Binary Tree node structure:
 
-    class BinaryTreeNode {
-    public : 
-        T data;
-        BinaryTreeNode<T> *left;
-        BinaryTreeNode<T> *right;
-
-        BinaryTreeNode(T data) {
-            this -> data = data;
-            left = NULL;
-            right = NULL;
-        }
-    };
-*/
-
-// using iteration - TC : O(h), SC : O(1)
-bool searchInBST(BinaryTreeNode<int> *root, int x) {
-    BinaryTreeNode<int> *temp = root;
-
-    while(temp != NULL) {
-        if(x == temp->data)
-            return true;
-        
-        if(x < temp->data)
-            temp = temp->left;
-        else
-            temp = temp->right;
+class Solution {
+public:
+    Solution() {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
     }
 
-    return false;
-}
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if(root == NULL || root->val == val) return root;
+        if(root->val > val) { // root->left me hai
+            return searchBST(root->left, val);
+        }
+        else { // root->right me hai
+            return searchBST(root->right, val);
+        }
+    }
+};
 
-// using recursion - TC : O(h), SC : O(n)
-/*
-bool searchInBST(BinaryTreeNode<int> *root, int x) {
-    // base case - 1
-    if(root == NULL)
-        return false;
-    
-    // base case - 2
-    if(x == root->data)
-        return true;
-    
-    // left part me
-    if(x < root->data)
-        return searchInBST(root->left, x);
-    // right part me
-    else
-        return searchInBST(root->right, x);
-
-}
-*/
 
 int main() {
     
