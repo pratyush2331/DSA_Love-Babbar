2b4d72b7b3e81ca4e8dd34495f6da3cc027bf949
Pratyush Raj <pratyush2331@gmail.com>
2024-05-04 00:55:34 +0530

    added another solution for Validate BST

--- 10. Tree/2. BST/L70_1_validateBST.cpp
+++ 10. Tree/2. BST/L70_1_validateBST.cpp
@@ -10,19 +10,13 @@ using namespace std;
 
 // 98. Validate Binary Search Tree
 // LeetCode : https://leetcode.com/problems/validate-binary-search-tree/
+// method-1
 class Solution {
     bool isBST(TreeNode* root, long min, long max) {
-        if(root == NULL)
-            return true;
-        
-        if(root->val > min && root->val < max) {
-            bool left = isBST(root->left, min, root->val);
-            bool right = isBST(root->right, root->val, max);
-            return left && right;
-        }
-        else {
-            return false;
-        }
+        if(root == NULL) return true;
+        bool left = isBST(root->left, min, root->val);
+        bool right = isBST(root->right, root->val, max);
+        return (root->val > min && root->val < max) && left && right;
     }
 
 public:
@@ -30,6 +24,28 @@ public:
         return isBST(root, LONG_MIN, LONG_MAX);
     }
 };
+
+// method-0
+/*
+class Solution {
+    bool inorderTraversal(TreeNode* root, long long& prev) {
+        if(root == NULL) return true;
+        bool left = inorderTraversal(root->left, prev);
+        if(prev != LONG_MIN && prev >= root->val) return false;
+        prev = root->val;
+        bool right = inorderTraversal(root->right, prev);
+        return left && right;
+    }
+
+public:
+    bool isValidBST(TreeNode* root) {
+        long long prev = LONG_MIN;
+        return inorderTraversal(root, prev);
+    }
+};
+*/
+
+
 
 // Partial BST
 // CodeStudio : https://www.codingninjas.com/codestudio/problems/validate-bst_799483?leftPanelTab=0
