27d4dfc1a61d8e9a7bf059146ef62b3d96265485
Pratyush Raj <pratyush2331@gmail.com>
2024-05-05 00:04:58 +0530

    optimized code structure

--- 10. Tree/2. BST/L71_4_constructBSTFromPreorder.cpp
+++ 10. Tree/2. BST/L71_4_constructBSTFromPreorder.cpp
@@ -1,46 +1,26 @@
 // 1008. Construct Binary Search Tree from Preorder Traversal
-// LeetCode : https://leetcode.com/problems/construct-binary-search-tree-from-preorder-traversal/description/
+// LeetCode : https://leetcode.com/problems/construct-binary-search-tree-from-preorder-traversal
 
 #include<iostream>
 using namespace std;
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-
-// TC : O(3n) = O(n), SC : O(n + h) = O(n)
+/*
+TC : O(3n) = O(n)
+SC : O(h) --> recursive stack space
+*/
 class Solution {
-    TreeNode* solve(vector<int> &preorder, int maxi, int &i) {
-        // base case - 1
-        if(i >= preorder.size())
-            return NULL;
-        
-        // base case - 2
-        if(preorder[i] > maxi) // = ki jaroorat nhi hai, as All the values of preorder are unique
-            return NULL;
-
+    TreeNode* buildBST(vector<int>& preorder, int& i, int upperBound) {
+        if(i == preorder.size() || preorder[i] > upperBound) return NULL;
         TreeNode* root = new TreeNode(preorder[i++]);
-
-        // left aur right ke liye call kardo
-        root->left = solve(preorder, root->val, i);
-        root->right = solve(preorder, maxi, i);
-
-        // root ko return kardo
+        root->left = buildBST(preorder, i, root->val);
+        root->right = buildBST(preorder, i, upperBound);
         return root;
     }
 
-    public:
+public:
     TreeNode* bstFromPreorder(vector<int>& preorder) {
         int i = 0;
-        return solve(preorder, INT_MAX, i);
+        return buildBST(preorder, i, INT_MAX);
     }
 };
 
