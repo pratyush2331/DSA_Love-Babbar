178fab80e99f8fbb0aa9271f6205035e95ad12a7
Pratyush Raj <pratyush2331@gmail.com>
2024-05-21 00:26:22 +0530

    solved 1 LC medium Graph Question

---
+++ 15. Graphs/X_3_NumberOfIslands.cpp
@@ -0,0 +1,59 @@
+// 200. Number of Islands
+// LeetCode : https://leetcode.com/problems/number-of-islands
+
+/*
+TC : O(mn + mn)
+SC : O(mn)
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+    vector<pair<int, int>> del = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}}; // {delRow, delCol}
+
+    void bfs(int i, int j, vector<vector<char>>& grid, vector<vector<bool>>& vis) {
+        queue<pair<int, int>> q;
+        vis[i][j] = 1;
+        q.push({i, j});
+        while(!q.empty()) {
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+
+            for(auto [delRow, delCol] : del) {
+                int newRow = row + delRow;
+                int newCol = col + delCol;
+                if(newRow >= 0 && newRow < grid.size() && newCol >= 0 && newCol < grid[0].size() 
+                && grid[newRow][newCol] == '1' && !vis[newRow][newCol]) {
+                    vis[newRow][newCol] = 1;
+                    q.push({newRow, newCol});
+                }
+            }
+        }
+    }
+
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<bool>> vis(m, vector<bool>(n, 0));
+        int cnt = 0;
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(grid[i][j] == '1' && !vis[i][j]) {
+                    cnt++;
+                    bfs(i, j, grid, vis);
+                }
+            }
+        }
+        return cnt;
+    }
+};
+
+
+int main() {
+    
+    return 0;
+}
