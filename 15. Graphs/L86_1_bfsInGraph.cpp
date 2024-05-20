073b6d35e1a9a9958cf046620c7466303b36df6a
Pratyush Raj <pratyush2331@gmail.com>
2024-05-20 13:28:29 +0530

    updated code structure

--- 15. Graphs/L86_1_bfsInGraph.cpp
+++ 15. Graphs/L86_1_bfsInGraph.cpp
@@ -3,84 +3,36 @@
 // CodeStudio [undirected disconnected graph] : https://www.codingninjas.com/codestudio/problems/bfs-in-graph_973002
 
 /*
-You are given an undirected and disconnected graph G(V,E) having V vertices numbered from 0 to V-1 and E edges.
-For each node, the correct order of printing the connected nodes will be sorted order.
-*/
-
-/*
-Sample Input:
-4 4
-
-0 1
-0 3
-1 2
-2 3
-
-Sample Output:
-0 1 3 2
-*/
-
-
-/*
-TC : O(V + E)
-SC : O(V + E)
+TC : O(V) + O(2E)
+SC : O(V)
 */
 
 #include <bits/stdc++.h>
 using namespace std;
 
 
-void getAdjList(vector<pair<int, int>>& edges, unordered_map<int, set<int>> &adjList) {
-    for(int i = 0; i < edges.size(); i++) {
-        int u = edges[i].first;
-        int v = edges[i].second;
-
-        adjList[u].insert(v);
-        adjList[v].insert(u);
-    }
-}
-
-void BFS(int node, unordered_map<int, set<int>>& adjList, unordered_map<int, bool> &visited, vector<int> &ans) {
-    queue<int> q;
-    q.push(node);
-    visited[node] = true;
-
-    while(!q.empty()) {
-        int frontNode = q.front();
-        q.pop();
-
-        // store frontNode into ans
-        ans.push_back(frontNode);
-
-        // traverse all neighbours of frontNode
-        for(auto i : adjList[frontNode]) { // traversing adjacency list of 'frontNode'
-            if(!visited[i]) {
-                visited[i] = true;
-                q.push(i);
+class Solution {
+  public:
+    vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+        vector<int> ans;
+        bool vis[V] = {0};
+        queue<int> q;
+        q.push(0);
+        vis[0] = 1;
+        while(!q.empty()) {
+            int front = q.front();
+            q.pop();
+            ans.push_back(front);
+            for(auto nbr : adj[front]) {
+                if(!vis[nbr]) {
+                    vis[nbr] = 1;
+                    q.push(nbr);
+                }
             }
         }
+        return ans;
     }
-}
-
-vector<int> BFS(int vertex, vector<pair<int, int>> edges) {
-    vector<int> ans;
-
-    unordered_map<int, set<int>> adjList; // we are using 'set' instead of 'list' because we want answer in sorted order
-    unordered_map<int, bool> visited; // to mark a particular node as visited after visiting that node
-
-    getAdjList(edges, adjList); // store adjacency list in 'adjList'
-
-    // for connected graph --> no need of for loop, directly call BFS() for starting node
-
-    // traverse all components of disconnected graph
-    for (int i = 0; i < vertex; i++) {
-        if(!visited[i]) {
-            BFS(i, adjList, visited, ans);
-        }
-    }
-
-    return ans;
-}
+};
 
 
 int main() {
--- 15. Graphs/L87_1_dfsInGraph.cpp
+++ 15. Graphs/L87_1_dfsInGraph.cpp
@@ -2,49 +2,35 @@
 // GFG [undirected connected graph] : https://practice.geeksforgeeks.org/problems/depth-first-traversal-for-a-graph/1
 // CodeStudio [undirected disconnected graph] : https://www.codingninjas.com/codestudio/problems/dfs-traversal_630462?leftPanelTab=0
 
+/*
+TC : O(V) + O(2E)
+SC : O(V)
+*/
+
 #include<iostream>
 using namespace std;
 
-void DFS(int node, unordered_map<int, set<int>>& adjList, unordered_map<int, bool>& visited, vector<int>& component) {
-    component.push_back(node);
-    visited[node] = true;
 
-    // go to depth
-    for(auto i : adjList[node]) {
-        if(!visited[i]) {
-            DFS(i, adjList, visited, component);
+class Solution {
+    void dfs(int node, vector<int> adj[], bool vis[], vector<int>& ans) {
+        vis[node] = 1;
+        ans.push_back(node);
+        for(auto nbr : adj[node]) {
+            if(!vis[nbr]) {
+                dfs(nbr, adj, vis, ans);
+            }
         }
     }
-}
-
-vector<vector<int>> depthFirstSearch(int V, int E, vector<vector<int>> &edges)
-{
-    // Write your code here
-    vector<vector<int>> ans;
-
-    // preparing adj list
-    unordered_map<int, set<int>> adjList;
-    for(int i = 0; i < E; i++) {
-        int u = edges[i][0];
-        int v = edges[i][1];
-
-        adjList[u].insert(v);
-        adjList[v].insert(u);
+    
+  public:
+    vector<int> dfsOfGraph(int V, vector<int> adj[]) {
+        vector<int> ans;
+        bool vis[V] = {0};
+        dfs(0, adj, vis, ans);
+        return ans;
     }
+};
 
-    // visited map
-    unordered_map<int, bool> visited;
-    // since, graph can be disconnected
-    for(int i = 0; i < V; i++) {
-        if(!visited[i]) {
-            vector<int> component;
-            DFS(i, adjList, visited, component);
-            ans.push_back(component);
-        }
-    }
-
-    return ans;
-}
 
 int main() {
     
