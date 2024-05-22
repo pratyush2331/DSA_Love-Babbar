88fa65e045e446cf890b0bfcdddaf3330245dd2b
Pratyush Raj <pratyush2331@gmail.com>
2024-05-22 19:17:53 +0530

    updated code structure

--- 15. Graphs/L88_1_cycleDetectionInUndirectedGraph.cpp
+++ 15. Graphs/L88_1_cycleDetectionInUndirectedGraph.cpp
@@ -1,94 +1,71 @@
 // Cycle Detection In Undirected Graph
-// CodeStudio : https://www.codingninjas.com/codestudio/problems/cycle-detection-in-undirected-graph_1062670
+// CodeStudio : https://www.geeksforgeeks.org/problems/detect-cycle-in-an-undirected-graph/1
+
+// can have disconnected components
 
 #include<bits/stdc++.h>
 using namespace std;
 
 
-// using BFS
-/*
-TC : O(V+E)
-SC : O(V+E)
-*/
-bool isCyclicBFS(int node, unordered_map<int, list<int>>& adjList, unordered_map<int, bool>& visited) { // TC: O(V + E)
-    unordered_map<int, int> parent; //SC: O(V)
-
-    queue<int> q;
-    q.push(node);
-    visited[node] = true;
-    parent[node] = -1;
-
-    while(!q.empty()) {
-        int frontNode = q.front();
-        q.pop();
-
-        for(auto i : adjList[frontNode]) {
-            if(!visited[i]) {
-                q.push(i);
-                visited[i] = true;
-                parent[i] = frontNode;
-            }
-            else if(visited[i] == true && i != parent[frontNode]) {
-                return true;
-            }
-        }
-    }
-
-    return false;
-}
-
-// using DFS
-/*
-TC : O(V+E)
-SC : O(V+E)
-*/
-bool isCyclicDFS(int node, int parent, unordered_map<int, list<int>>& adjList, unordered_map<int, bool>& visited) { // TC: O(V+E)
-    visited[node] = true;
-
-    for(auto i : adjList[node]) {
-        if(!visited[i]) {
-            bool ans = isCyclicDFS(i, node, adjList, visited);
-            if(ans)
-                return true;
-        }
-        else if(visited[i] == true && i != parent) {
-            return true;
-        }
-    }
-
-    return false;
-}
-
-string cycleDetection (vector<vector<int>>& edges, int n, int m)
-{
-    // Write your code here.
-
-    // prepare adjList
-    unordered_map<int, list<int>> adjList; // SC: O(E)
-    for(int i = 0; i < m; i++) {
-        int u = edges[i][0];
-        int v = edges[i][1];
-
-        adjList[u].push_back(v);
-        adjList[v].push_back(u);
-    }
-
-    // visited map
-    unordered_map<int, bool> visited; // SC: O(V)
-
-    // to handle disconnected components
-    for(int i = 0; i < n; i++) {
-        if(!visited[i]) {
-            // bool ans = isCyclicBFS(i, adjList, visited);
-            bool ans = isCyclicDFS(i, -1, adjList, visited);
+class Solution {
+    // using BFS
+    /*
+    TC : O(V + 2E) + O(V)
+    SC : O(V)
+    */
+    bool BFS(int src, vector<int> adj[], vector<bool>& vis) {
+        vis[src] = 1;
+        queue<pair<int, int>> q; // SC:O(V)
+        q.push({src, -1});
+        while(!q.empty()) { // TC : O(V + 2E) --> for one component
+            int node = q.front().first;
+            int par = q.front().second;
+            q.pop();
             
-            if(ans == true)
-                return "Yes";
+            for(auto nbr : adj[node]) {
+                if(!vis[nbr]) {
+                    vis[nbr] = 1;
+                    q.push({nbr, node});
+                }
+                else if(nbr != par) {
+                    return true;
+                }
+            }
         }
+        return false;
     }
 
-    return "No";
-}
+
+    // using DFS
+    /*
+    TC : O(V + 2E) + O(V)
+    SC : O(V) + O(V) = O(V)
+    */
+    bool DFS(int node, int par, vector<int> adj[], vector<bool>& vis) {
+        vis[node] = 1;
+        for(auto nbr : adj[node]) {
+            if(!vis[nbr]) {
+                if(DFS(nbr, node, adj, vis)) return true;
+            }
+            else if(nbr != par) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    
+    public:
+    bool isCycle(int V, vector<int> adj[]) {
+        vector<bool> vis(V, 0);
+        for(int i = 0; i < V; i++) { // TC:O(V)
+            if(!vis[i]) {
+                if(BFS(i, adj, vis)) return true;
+            }
+        }
+        return false;
+    }
+};
 
 
 int main() {
--- test.java
+++ test.java
@@ -1,7 +1,3 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
 public class test {
     public static void main(String[] args) {
         
