26d7dd6d6954758a1091416f575d1658fef0b687
Pratyush Raj <pratyush2331@gmail.com>
2024-11-09 11:19:44 +0530

    added TC & SC

--- XX. AAA_Platform/LeetCode/3_LeetCode_POTD/2024_Nov_08.cpp
+++ XX. AAA_Platform/LeetCode/3_LeetCode_POTD/2024_Nov_08.cpp
@@ -5,17 +5,21 @@
 using namespace std;
 
 
+/*
+TC : O(n.logn)
+SC : O(n)
+*/
 class Solution {
 public:
     vector<int> getMaximumXor(vector<int>& nums, int maximumBit) {
-        vector<int> ans;
+        vector<int> ans; // SC: O(1)
         int maxNum = (1 << maximumBit) - 1;
         int xorSum = 0;
-        for(auto ele : nums) {
+        for(auto ele : nums) { // TC: O(n)
             xorSum ^= ele;
             ans.emplace_back(maxNum ^ xorSum);
         }
-        reverse(ans.begin(), ans.end());
+        reverse(ans.begin(), ans.end()); // TC: O(n.logn), SC: O(1)
         return ans;
     }
 };
