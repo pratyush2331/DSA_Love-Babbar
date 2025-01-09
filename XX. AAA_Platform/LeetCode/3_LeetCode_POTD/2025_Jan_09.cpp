236bc41adc3b047a9d2ea4213b414def9302cb90
Pratyush Raj <pratyush2331@gmail.com>
2025-01-09 10:49:14 +0530

    solved today's POTD

---
+++ XX. AAA_Platform/LeetCode/3_LeetCode_POTD/2025_Jan_09.cpp
@@ -0,0 +1,45 @@
+// 2185. Counting Words With a Given Prefix
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// Method - 1
+/*
+TC : O(n * m)
+SC : O(1)
+*/
+class Solution {
+public:
+    int prefixCount(vector<string>& words, string pref) {
+        int cnt = 0;
+        for(auto& word : words) if(word.find(pref) == 0) cnt++;
+        return cnt;
+    }
+};
+
+
+// Method - 0
+/*
+TC : O(n * m)
+SC : O(1)
+*/
+class Solution {
+public:
+    int prefixCount(vector<string>& words, string pref) {
+        int cnt = 0;
+        for(auto& word : words) {
+            if(word.size() < pref.size()) continue;
+            int i;
+            for(i = 0; i < pref.size() && word[i] == pref[i]; i++);
+            if(i == pref.size()) cnt++;
+        }
+        return cnt;
+    }
+};
+
+
+int main() {
+    
+    return 0;
+}
