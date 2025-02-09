fbc5a8560baf8ba4d1d3c641e39d1f7722e220c3
Pratyush Raj <pratyush2331@gmail.com>
2025-02-10 02:30:36 +0530

    solved one HashMap Q

--- 12. HashMap/X_6_1814_CountNicePairsInAnArray.cpp
+++ 12. HashMap/X_6_1814_CountNicePairsInAnArray.cpp
@@ -5,9 +5,14 @@
 using namespace std;
 
 
+// method : using HashMap
+/*
+TC : O(n)
+SC : O(n)
+*/
 class Solution {
     int MOD = 1e9+7;
-    int rev(int num) {
+    int rev(int num) { // TC:O(9)
         string str = to_string(num);
         reverse(str.begin(), str.end());
         return stoi(str);
@@ -15,12 +20,12 @@ class Solution {
 
 public:
     int countNicePairs(vector<int>& nums) {
-        unordered_map<int, int> mp;
-        for(int& ele : nums) {
+        unordered_map<int, int> mp; // SC:O(n)
+        for(int& ele : nums) { // TC:O(n*9) = O(n)
             mp[ele - rev(ele)]++;
         }
         int np = 0;
-        for(auto& ele : mp) np = (np + (1LL * ele.second * (ele.second - 1) / 2) % MOD) % MOD;
+        for(auto& ele : mp) np = (np + (1LL * ele.second * (ele.second - 1) / 2) % MOD) % MOD; // TC:O(n)
         return np % MOD;
     }
 };
