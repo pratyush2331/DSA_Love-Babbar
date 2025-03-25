c35a0ee59097eaf32215930dd8c4429fdcfdf3a8
Pratyush Raj <pratyush2331@gmail.com>
2025-03-26 00:44:01 +0530

    added TC & SC & line sweep approach with comments

--- 17. Greedy/XX_3_3169_CountDaysWithoutMeetings.cpp
+++ 17. Greedy/XX_3_3169_CountDaysWithoutMeetings.cpp
@@ -5,6 +5,50 @@
 using namespace std;
 
 
+// method-1 : using Line Sweep logic
+/*
+TC : O(n.logn)
+SC : O(1) // not considering sorting space
+*/
+class Solution {
+public:
+    int countDays(int days, vector<vector<int>>& meetings) {
+        map<int, int> dayMap;
+        int prefixSum = 0, freeDays = 0, previousDay = days;
+
+        for (auto& meeting : meetings) {
+            // Set first day of meetings
+            previousDay = min(previousDay, meeting[0]);
+
+            // Process start and end of meeting
+            dayMap[meeting[0]]++;
+            dayMap[meeting[1] + 1]--;
+        }
+
+        // Add all days before the first day of meetings
+        freeDays += (previousDay - 1);
+        for (auto& day : dayMap) {
+            int currentDay = day.first;
+            // Add current range of days without a meeting
+            if (prefixSum == 0) {
+                freeDays += currentDay - previousDay;
+            }
+            prefixSum += day.second;
+            previousDay = currentDay;
+        }
+
+        // Add all days after the last day of meetings
+        freeDays += days - previousDay + 1;
+        return freeDays;
+    }
+};
+
+
+// method-0 : using merge intervals logic
+/*
+TC : O(n.logn)
+SC : O(1) // not considering sorting space
+*/
 class Solution {
 public:
     int countDays(int days, vector<vector<int>>& meetings) {
