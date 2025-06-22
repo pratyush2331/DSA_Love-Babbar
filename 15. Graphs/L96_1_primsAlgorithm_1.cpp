d3f647b0aa5905e2839fd6a38900ad6548ef97c7
Pratyush Raj <pratyush2331@gmail.com>
2025-06-22 07:48:19 +0530

    added code fror mst

--- 15. Graphs/L96_1_primsAlgorithm_1.cpp
+++ 15. Graphs/L96_1_primsAlgorithm_1.cpp
@@ -6,6 +6,7 @@
 using namespace std;
 
 
+// for getting sum
 /*
 TC : O(E.logE)
 SC : O(V + E)
@@ -40,6 +41,51 @@ class Solution {
             }
         }
         return sum;
+    }
+};
+
+
+// for getting sum & mst
+/*
+TC : O(E.logE)
+SC : O(V + E)
+*/
+class Solution {
+  public:
+    // Function to find sum of weights of edges of the Minimum Spanning Tree.
+    vector<pair<int, int>> spanningTree(int V, vector<vector<int>> adj[]) {
+        // min-heap: {weight, node, parent}, SC: O(E)
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<>> pq;
+        // to store the edges of the mst, {parent, node}
+        vector<pair<int, int>> mst; // SC:O(V)
+        vector<bool> vis(V, 0); // SC: O(V)
+        pq.push({0, {0, -1}});
+        int sum = 0;
+        // TC: O(E.logE) + O(E.logE) = O(E.logE)
+        while(!pq.empty()) { // will run for E times
+            int wt = pq.top().first; // TC:O(logE)
+            int node = pq.top().second.first;
+            int parent = pq.top().second.second;
+            pq.pop();
+            
+            // if node not visited
+            // add it to the mst, and mark visited
+            if(!vis[node]) {
+                vis[node] = 1;
+                if(parent != -1) { // if parent is not -1
+                    mst.push_back({parent, node}); // add edge to mst
+                    sum += wt; // add weight to sum
+                }
+                for(auto nbr : adj[node]) { // will run for E.logE times
+                    int nbrNode = nbr[0];
+                    int nbrWt = nbr[1];
+                    if(!vis[nbrNode]) {
+                        pq.push({nbrWt, {nbrNode, node}});
+                    }
+                }
+            }
+        }
+        return mst;
     }
 };
 
