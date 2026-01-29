0bd9112ab1421d2fb71afae7575f5c31409996fe
Pratyush Raj <pratyush2331@gmail.com>
2026-01-29 23:51:24 +0530

    code refinement

--- 02. Arrays/1. Integer Arrays/2. Searching/2. Binary Search/LB_l15_HW1_EKO_SPOJ.cpp
+++ 02. Arrays/1. Integer Arrays/2. Searching/2. Binary Search/LB_l15_HW1_EKO_SPOJ.cpp
@@ -2,6 +2,22 @@
 // SPOJ : https://www.spoj.com/problems/EKO/
 
 /*
+Lumberjack Mirko needs to chop down M metres of wood. It is an easy job for him since he has a nifty new woodcutting machine 
+that can take down forests like wildfire. However, Mirko is only allowed to cut a single row of trees.
+
+Mirko‟s machine works as follows: Mirko sets a height parameter H (in metres), and the machine raises a giant sawblade 
+to that height and cuts off all tree parts higher than H (of course, trees not higher than H meters remain intact). 
+Mirko then takes the parts that were cut off. For example, if the tree row contains trees with heights of 20, 15, 10, 
+and 17 metres, and Mirko raises his sawblade to 15 metres, the remaining tree heights after cutting will be 15, 15, 10, 
+and 15 metres, respectively, while Mirko will take 5 metres off the first tree and 2 metres off the fourth tree 
+(7 metres of wood in total).
+
+Mirko is ecologically minded, so he doesn‟t want to cut off more wood than necessary. That‟s why he wants to set his sawblade 
+as high as possible. Help Mirko find the maximum integer height of the sawblade that still allows him to cut off at least 
+M metres of wood.
+*/
+
+/*
 sample i/p 1:
 4 7
 20 15 10 17
--- 02. Arrays/1. Integer Arrays/2. Searching/2. Binary Search/LB_l15_HW2_PRATA_SPOJ.cpp
+++ 02. Arrays/1. Integer Arrays/2. Searching/2. Binary Search/LB_l15_HW2_PRATA_SPOJ.cpp
@@ -4,7 +4,14 @@
 // Time Complexity: O(n^2 logn)
 
 /*
-IEEE is having its AGM next week and the president wants to serve cheese prata after the meeting. The subcommittee members are asked to go to food connection and get P (P<=1000) pratas packed for the function. The stall has L cooks (L<=50) and each cook has a rank R (1<=R<=8). A cook with a rank R can cook 1 prata in the first R minutes 1 more prata in the next 2R minutes, 1 more prata in 3R minutes and so on(he can only cook a complete prata) ( For example if a cook is ranked 2.. he will cook one prata in 2 minutes one more prata in the next 4 mins an one more in the next 6 minutes hence in total 12 minutes he cooks 3 pratas in 13 minutes also he can cook only 3 pratas as he does not have enough time for the 4th prata). The webmaster wants to know the minimum time to get the order done. Please write a program to help him out.
+IEEE is having its AGM next week and the president wants to serve cheese prata after the meeting. 
+The subcommittee members are asked to go to food connection and get P (P<=1000) pratas packed for the function. 
+The stall has L cooks (L<=50) and each cook has a rank R (1<=R<=8). A cook with a rank R can cook 1 prata in the 
+first R minutes 1 more prata in the next 2R minutes, 1 more prata in 3R minutes and so on(he can only cook a complete prata) 
+( For example if a cook is ranked 2.. he will cook one prata in 2 minutes one more prata in the next 4 mins an one more 
+in the next 6 minutes hence in total 12 minutes he cooks 3 pratas in 13 minutes also he can cook only 3 pratas as he 
+does not have enough time for the 4th prata). The webmaster wants to know the minimum time to get the order done. 
+Please write a program to help him out.
 */
 
 /*
