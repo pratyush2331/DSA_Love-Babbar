4480b48d574c1fb7c600eeb5cb60723a37916c44
pratyush2331 <pratyush2331@gmail.com>
2026-09-06 15:34:12 +0530

    code update

--- 02. Arrays/3. 2-D Arrays/Matrix Basics/23_HW_1_rotateImage.cpp
+++ 02. Arrays/3. 2-D Arrays/Matrix Basics/23_HW_1_rotateImage.cpp
@@ -1,4 +1,5 @@
-// LeetCode-48 : Rotate Image
+// 48. Rotate Image
+// LeetCode : https://leetcode.com/problems/rotate-image/
 /*
 sample i/p - 1:
 1 2 3
@@ -28,40 +29,100 @@ sample o/p - 2:
 
 using namespace std;
 
-// Method - 1
 /*
-void rotate(vector<vector<int>>& matrix) {
-    int n = matrix.size();
-    
-    // changing symmetrically from middle row
-    for(int i = 0; i < n/2; i++) {
-        for(int j = 0; j < n; j++) {
-            swap(matrix[i][j], matrix[n-1-i][j]);
-        }
-    }
-    
-    // taking transpose
-    for(int i = 0; i < n; i++) {
-        for(int j = i; j < n; j++) {
-            swap(matrix[i][j], matrix[j][i]);
-        }
-    }
-}
+TC : O(n^2)
+SC : O(1)
 */
+// method-0 : transpose + reverse approach
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        // 1. transpose
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < i; j++) {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
 
-// Method - 2
-void rotate(vector<vector<int>>& matrix) {
-    int n = matrix.size();
-    for (int i = 0; i < (n + 1) / 2; i ++) {
-        for (int j = 0; j < n / 2; j++) {
-            int temp = matrix[n - 1 - j][i];
-            matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1];
-            matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 -i];
-            matrix[j][n - 1 - i] = matrix[i][j];
-            matrix[i][j] = temp;
+        // 2. reverse rows
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n/2; j++) {
+                swap(matrix[i][j], matrix[i][n-j-1]);
+            }
         }
     }
-}
+};
+
+// method-1 : quadrant rotation approach
+/*
+[(0,0), (0,1), (0,2), (0,3)]
+[(1,0), (1,1), (1,2), (1,3)]
+[(2,0), (2,1), (2,2), (2,3)]
+[(3,0), (3,1), (3,2), (3,3)]
+
+(0,1) --> (2,0)
+(i,j) --> (n-j-1,i)
+
+(2,0) --> (3,2)
+(n-j-1,i) --> (n-i-1,n-j-1)
+
+(3,2) --> (1,3)
+(n-i-1,n-j-1) --> (j,n-i-1)
+
+(1,3) --> (0,1)
+(j,n-i-1) --> (i,j) {temp}
+*/
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        for(int i = 0; i < n/2; i++) {
+            for(int j = 0; j < (n+1)/2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n-j-1][i];
+                matrix[n-j-1][i] = matrix[n-i-1][n-j-1];
+                matrix[n-i-1][n-j-1] = matrix[j][n-i-1];
+                matrix[j][n-i-1] = temp;
+            }
+        }
+    }
+};
+
+// method-2 : 4-way cyclic rotation approach
+/*
+[(0,0), (0,1), (0,2), (0,3)]
+[(1,0), (1,1), (1,2), (1,3)]
+[(2,0), (2,1), (2,2), (2,3)]
+[(3,0), (3,1), (3,2), (3,3)]
+
+(0,1) --> (2,0)
+(i,j) --> (n-j-1,i)
+
+(2,0) --> (3,2)
+(n-j-1,i) --> (n-i-1,n-j-1)
+
+(3,2) --> (1,3)
+(n-i-1,n-j-1) --> (j,n-i-1)
+
+(1,3) --> (0,1)
+(j,n-i-1) --> (i,j) {temp}
+*/
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        for(int i = 0; i < n/2; i++) {
+            for(int j = i; j < n-i-1; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n-j-1][i];
+                matrix[n-j-1][i] = matrix[n-i-1][n-j-1];
+                matrix[n-i-1][n-j-1] = matrix[j][n-i-1];
+                matrix[j][n-i-1] = temp;
+            }
+        }
+    }
+};
 
 void printArray(vector<vector<int>> matrix) {
     int nRows = matrix.size();
